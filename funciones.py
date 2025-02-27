9efb05ce316c3a9c81292c22e11d26e2581ecd14
KhaelF0ns <mhatt.fons@gmail.com>
2025-02-27 15:08:56 +0000

    Funcion es_primo_a_mano(n) añadida

--- funciones.py
+++ funciones.py
@@ -1,4 +1,9 @@
-def es_primo(n):
+from sympy import isprime
+
+def es_primo(int: n) -> bool:
+    return isprime(n
+    
+def es_primo_a_mano(int: n) -> bool:
     if n <= 1:
         return False
     for i in range(2, int(n**0.5) + 1):
