3b5d1a83af6a49d391eb7065f6aa4af7e0061c18
KhaelF0ns <mhatt.fons@gmail.com>
2025-02-27 15:31:17 +0000

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,9 +1,9 @@
 from sympy import isprime
 
-def es_primo(int: n) -> bool:
-    return isprime(n
+def es_primo(n: int) -> bool:
+    return isprime(n)
     
-def es_primo_a_mano(int: n) -> bool:
+def es_primo_a_mano(n: int) -> bool:
     if n <= 1:
         return False
     for i in range(2, int(n**0.5) + 1):
